764cd945aae8ed7040cab21bb466c8fcd4fe30f1
Alexis Bellido <a@zinibu.com>
2020-10-16 10:25:11 -0400

    quick test to read a CSV file

--- django/project/rocketlaunch/management/commands/import_companies.py
+++ django/project/rocketlaunch/management/commands/import_companies.py
@@ -1,1 +1,7 @@
 # TODO Django management command to import companies
+
+import csv
+with open('/tmp/csv/rocket_companies.csv') as csvfile:
+    reader = csv.DictReader(csvfile)
+    for row in reader:
+        print(row)
